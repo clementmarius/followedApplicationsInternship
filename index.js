c0770460b47227abbac5c6c443035d5c52304860
stradimarius <mariusclement68@gmail.com>
2024-09-30 12:59:10 +0200

    First commit - initial project upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+// Middleware pour analyser les données des formulaires (si nécessaire)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');  
+});
+
+app.post('/submit-form', (req, res) => {
+    res.send('Form submitted');
+});
+
+// Middleware pour logger les requêtes
+app.use((req, res, next) => {
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);  
+});
