fa092b9143836bc80140e57b58e262df0310f4cc
stradimarius <mariusclement68@gmail.com>
2024-09-30 18:38:11 +0200

    Creation du service

--- services/userService.js
+++ services/userService.js
@@ -2,3 +2,29 @@ const prisma = require('../libs/prisma');
 const bcrypt = require('bcrypt');
 const { sendEmail: brevoSendEmail } = require('../brevo');
 const { email } = require('../config');
+
+async function createUserProfile(userData, ProfileData) {
+    try {
+
+        const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                email: userData.email,
+                password: hashedPassword,
+                profile: {
+                    create:{
+                        firstName: ProfileData.firstName,
+                        lastName: ProfileData.lastName,
+                    },
+                },
+            },
+        });
+
+        return newUser;
+
+    } catch (error) {
+        console.error('Error creating user profile:', error);
+        throw new Error('Could not create user profile');
+    }
+}
