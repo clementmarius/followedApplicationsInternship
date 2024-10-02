1f7632dcbbc95c4a02300b1a9c4e60008d7ed1f6
stradimarius <mariusclement68@gmail.com>
2024-10-02 11:44:32 +0200

    Routes added

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const jobController = require('../controllers/jobControllers');
+
+const router = express.Router();
+
+router.post('/createJob',jobController.createJob);
+
+module.exports = router;
