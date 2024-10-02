fac0083ce2d86525581abd777f76b11177055aac
stradimarius <mariusclement68@gmail.com>
2024-10-02 11:48:39 +0200

    Routes added

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -5,5 +5,10 @@ const jobController = require('../controllers/jobControllers');
 const router = express.Router();
 
 router.post('/createJob',jobController.createJob);
+/* router.get('/:id/getJobById',jobController.findJobById);
+router.get('/getAllJobId', jobController.findAllJobById);
+router.put('/updateJob', jobController.updateJob);
+router.delete('/:id/deleteJob',jobController.removeJob); */
+
 
 module.exports = router;
