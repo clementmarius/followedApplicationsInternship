277433bea6be582685b4cb38b5518fee57913fdd
stradimarius <mariusclement68@gmail.com>
2024-10-03 12:21:38 +0200

    new feats into the application job

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const applicationController = require('../controllers/applicationController');
+const applicationController = require('../controllers/applicationControllers');
 const authenticateToken = require('../middlewares/authMiddleware'); 
 
-router.post('/createApplication', authenticateToken, applicationController.createApplication);
+router.post('/createApplication', authenticateToken, applicationController.formingApplication);
 
 
 module.exports = router;
