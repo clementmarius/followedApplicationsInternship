0c738813a9c232397858ece58895c39a2bf681b6
stradimarius <mariusclement68@gmail.com>
2024-10-03 13:04:01 +0200

    auth request good

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/login', authController.loginUser);
+router.post('/auth', authController.loginUser);
 
 module.exports = router;
