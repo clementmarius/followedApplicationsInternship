b7c9f194ecae072ab20cd93049c2e226b631f4da
stradimarius <mariusclement68@gmail.com>
2024-10-03 12:42:23 +0200

    new auth service feats

---
+++ services/authService.js
@@ -0,0 +1,27 @@
+const prisma = require('../prismaClient');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const loginUser = async (email, password) => {
+
+    const user = await prisma.user.findUnique({
+        where: { email }
+    });
+
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        throw new Error('Invalid password');
+    }
+
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    return token;
+};
+
+module.exports = { loginUser };
