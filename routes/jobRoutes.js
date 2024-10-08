0ff7f7827c3948f71e4200b2515312e1041370eb
stradimarius <mariusclement68@gmail.com>
2024-10-08 15:49:17 +0200

    Maps

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const jobController = require('../controllers/jobControllers');
+const mapsController = require('../controllers/mapsController');
 
 const router = express.Router();
 
 router.post('/createJob',jobController.createJob);
+router.get('/map', mapsController.searchMaps);
 router.get('/filterAndSortJobs', jobController.filterAndSortJobs);
 router.get('/:id/getJobById',jobController.findJobById);
 router.get('/getAllJobId', jobController.getAllJobs);
