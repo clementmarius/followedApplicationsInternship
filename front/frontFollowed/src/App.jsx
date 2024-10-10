4ab0e2591c1e277777b7decf58933599da2e98ad
stradimarius <mariusclement68@gmail.com>
2024-10-10 11:26:50 +0200

    Install front react

--- front/frontFollowed/src/App.jsx
+++ front/frontFollowed/src/App.jsx
@@ -1,6 +1,12 @@
+/* const title = "Hello World"; */
+import './index.css';
 
 function App() {
-  return "Bonjour"
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
+      </>
+  );
 }
 
-export default App
+export default App;
