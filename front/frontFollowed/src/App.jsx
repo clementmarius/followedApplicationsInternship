9b21cc306e1e90ec49f9a75cefdc325b63340a8a
stradimarius <mariusclement68@gmail.com>
2024-10-14 11:40:04 +0200

    front

--- front/frontFollowed/src/App.jsx
+++ front/frontFollowed/src/App.jsx
@@ -1,12 +1,30 @@
 /* const title = "Hello World"; */
 import './index.css';
 
-function App() {
+/* function App() {
   return (
     <>
       <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
       </>
   );
-}
+} */
+
+  function App() {
+    const [count, setCount] = useState(0);
+  
+    const fetchApi = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api");
+        console.log(response.data.fruits);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données : ", error);
+      }
+    };
+  
+  
+    useEffect(() => {
+      fetchApi();
+    }, []);
+  }
 
 export default App;
--- front/frontFollowed/src/main.jsx
+++ front/frontFollowed/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
+
