5247077f5b2086e036e4d18e1cef331464d92752
stradimarius <mariusclement68@gmail.com>
2024-11-04 14:56:10 +0100

    start React

--- front/frontFollowed/src/App.jsx
+++ front/frontFollowed/src/App.jsx
@@ -1,40 +1,43 @@
 import { useContext } from "react";
 import { StoreContext } from "./store";
-import { Routes, Route, Link, NavLink } from "react-router-dom"; 
+import { Routes, Route, Link, NavLink, Outlet } from "react-router-dom";
 import LoginButton from "./components/LoginButton";
 import RegisterButton from "./components/RegisterButton";
 import { Single } from "./pages/RegisterPage";
 
-function App() {
+function Root() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen space-x-4">
-        <LoginButton />
-        <Link to="/register">
-          <RegisterButton />
-        </Link>
-
-
-        <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              Home page
-              <nav>
-                <NavLink to="/contact">Contact</NavLink>{" "}
-                <NavLink to="/blog">Blog</NavLink>
-              </nav>
-            </div>
-          }
-        />
-        <Route path="/register" element={<Single />} />
-        {/* Ajoutez d'autres routes ici si nécessaire */}
-      </Routes>
+      <header>
+        <nav className="flex space-x-4">
+          <NavLink to="/" className="text-blue-500">Home</NavLink>
+          <NavLink to="/blog" className="text-blue-500">Blog</NavLink>
+          <NavLink to="/contact" className="text-blue-500">Contact</NavLink>
+        </nav>
+      </header>
+      <div className="container my-4">
+        <Outlet />
       </div>
-
-      
     </>
+  );
+}
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen space-y-4 bg-gray-100">
+      <LoginButton />
+      <Link to="/register">
+        <RegisterButton />
+      </Link>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<div className="text-center">Home page</div>} />
+          <Route path="blog" element={<div className="text-center">Blog</div>} />
+          <Route path="contact" element={<div className="text-center">Contact</div>} />
+        </Route>
+        <Route path="/register" element={<Single />} />
+      </Routes>
+    </div>
   );
 }
 
--- front/frontFollowed/src/main.jsx
+++ front/frontFollowed/src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { TodoStoreProvider } from "./store";
-import LoginButton from "./components/LoginButton";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <TodoStoreProvider>
