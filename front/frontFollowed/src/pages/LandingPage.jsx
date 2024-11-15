ef9065654baa03118683edc8fb4efe681989e450
stradimarius <mariusclement68@gmail.com>
2024-11-15 11:46:06 +0100

    start React

--- front/frontFollowed/src/pages/LandingPage.jsx
+++ front/frontFollowed/src/pages/LandingPage.jsx
@@ -1,21 +1,26 @@
-//Utilisation du fetch pour importer la route profile/me 
+//Utilisation du fetch pour importer la route profile/me
 
+import { useState } from "react";
 
 // LandingPage.jsx
 const LandingPage = () => {
+  const [name, email] = useState("");
 
-const displayProfile = async () => {
-  console.log("display profile");
-  
-}
+  const displayProfile = async () => {
+    console.log("display profile");
+  };
 
   return (
     <div>
       <h1>Welcome on your user account</h1>
       <p>You are logged in to your account.</p>
       <h2>Your personnal informations</h2>
-      <p><strong>Name :</strong> </p>
-      <p><strong>Mail :</strong> </p>
+      <p>
+        <strong>Name :</strong>{" "}
+      </p>
+      <p>
+        <strong>Mail :</strong>{" "}
+      </p>
     </div>
   );
 };
