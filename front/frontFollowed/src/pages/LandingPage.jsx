5ef74d95da2541b8365c99162740865062367adc
stradimarius <mariusclement68@gmail.com>
2024-11-19 13:44:33 +0100

    start React

--- front/frontFollowed/src/pages/LandingPage.jsx
+++ front/frontFollowed/src/pages/LandingPage.jsx
@@ -23,6 +23,7 @@ const LandingPage = () => {
       <h1>Welcome on your user account</h1>
       <p>You are logged in to your account.</p>
       <br />
+      <p>More coming soon</p>
       <h2>Your personnal informations</h2>
       <p value={name}
       onChange={(e) => setName(e.target.value)}
